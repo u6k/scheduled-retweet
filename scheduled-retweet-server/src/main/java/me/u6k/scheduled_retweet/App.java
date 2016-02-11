af7d825e8cbca9b9e51506deb249c330b7c500ea
u6k <u6k.apps@gmail.com>
2016-02-12 05:49:57 +0900

    Spring Bootアプリケーションとしての体裁を整備して、起動することを確認しました。

--- scheduled-retweet-server/src/main/java/me/u6k/scheduled_retweet/App.java
+++ scheduled-retweet-server/src/main/java/me/u6k/scheduled_retweet/App.java
@@ -1,10 +1,14 @@
 
 package me.u6k.scheduled_retweet;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SpringApplication.run(App.class, args);
     }
 
 }
